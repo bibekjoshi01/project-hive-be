a5e616d18dd1d9fe001a8aeace78901f72073ebe
Bibek Joshi <bibekjoshi34@gmail.com>
2025-07-07 13:04:26 +0545

    fix: bug fixed

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from app.database import db_pool
 from app.config import settings
+from contextlib import asynccontextmanager
 
-app = FastAPI(title=settings.app_name, version=settings.app_version)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    if db_pool:
+        db_pool.closeall()
+
+
+app = FastAPI(lifespan=lifespan, title=settings.app_name, version=settings.app_version)
 
 
 @app.get("/health", tags=["Site"])
 async def health_check():
     return {"status": "ok"}
-
-
-@app.add_event_handler("shutdown")
-def shutdown_event():
-    if db_pool:
-        db_pool.closeall()
