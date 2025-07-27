6ee3f560a5b819ae624d64d97370928f2c9b7c63
Bibek Joshi <bibekjoshi34@gmail.com>
2025-07-27 21:27:08 +0545

    deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-DB_HOST=${DB_HOST:-db}
-
-echo "Waiting for db ($DB_HOST)…"
-while ! nc -z "$DB_HOST" 5432; do
-  sleep 1
-done
-echo "Database is ready!"
+echo "Starting FastAPI app..."
 
 alembic upgrade head
 
